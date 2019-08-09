96539e1a3955ba16731d4128e533cf807298cf86
Shane T Odlum <shane.odlum@consensys.net>
2019-08-09 12:00:56 +0100

    * Use prettier instead of airbnb (per Paris) * Add scripts / standardise script names * Lint according to new rules * Add package-lock.json to version control * Update dockerfile to newer node version

--- app.js
+++ app.js
@@ -1,28 +1,17 @@
 const app = require('fastify')();
 const autoLoad = require('fastify-autoload');
 const path = require('path');
-const logger = require('./config/winston');
 
 // FIXME Register route from file
 // app.register(require('./routes/healthcheck'));
 // This loads all plugins defined in /routes
 app.register(autoLoad, {
   dir: path.join(__dirname, 'routes'),
-  options: Object.assign({ prefix: '/api' }, {}),
+  options: Object.assign({ prefix: '/api' }, {})
 });
 
 app.get('/', (req, reply) => {
   reply.send({ hello: 'world' });
 });
-
-// Run the server
-app.listen(3000, (err, address) => {
-  if (err) {
-    logger.error(err);
-    process.exit(1);
-  }
-  logger.info(`server listening on ${address}`);
-});
-
 
 module.exports = app;
--- config/winston.js
+++ config/winston.js
@@ -9,27 +9,27 @@ const options = {
     json: true,
     maxsize: 5242880, // 5MB
     maxFiles: 5,
-    colorize: false,
+    colorize: false
   },
   console: {
     level: 'info',
     handleExceptions: true,
     json: false,
-    colorize: true,
-  },
+    colorize: true
+  }
 };
 
 // Define a custom format of the log messages
 const logFormat = winston.format.combine(
   winston.format.timestamp({ format: 'DD-MM-YYYY HH:mm:ss' }),
-  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`),
+  winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
 );
 
 const logger = winston.createLogger({
   transports: [
-    new winston.transports.Console({ format: logFormat, options: options.console }),
+    new winston.transports.Console({ format: logFormat, options: options.console })
     // new winston.transports.File(options.file), // Uncomment to have file logs enabled
-  ],
+  ]
 });
 
 module.exports = logger;
